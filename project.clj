5ac10ab0f7ca4dc03d44cc9ba0be70fbc590ec9c
stm <stmataic@gmail.com>
2015-09-29 20:12:23 +0200

    project settings

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject svm "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+(defproject svm "1.0.0"
+  :description "Sched Virtual Machine command line interface"
+  :url "https://github.com/arkon108/svm"
+  :dependencies [[org.clojure/clojure "1.6.0"] [org.clojure/tools.cli "0.3.3"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :aot [svm.core]
+  :aot :all
   :main svm.core)
--- src/svm/core.clj
+++ src/svm/core.clj
@@ -1,6 +1,10 @@
 (ns svm.core
   (:gen-class))
+(require '[clojure.tools.cli :refer [cli]])
 
-(defn -main
-  []
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [[opts args banner] (cli args
+                                ["-h" "--help" "Print this help" :default false :flag true]
+                                ["-p" "--port" "App doesn't care about ports"])]
+    (when (or (:help opts))
+      (println banner))))
